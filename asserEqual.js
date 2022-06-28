945d7d9718610bdc56ab1189d0f618d62a27fb78
Sszyh <zhuyer@outlook.com>
2022-06-27 19:27:36 -0700

    set a assertion function

---
+++ asserEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🎉🎉🎉Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("😈😈😈Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(1, 2);
