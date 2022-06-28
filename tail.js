2692866c92c726765d10f4cc0c6909478f5e7961
Sszyh <zhuyer@outlook.com>
2022-06-27 20:19:51 -0700

    add tail to return new string

---
+++ tail.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🎉🎉🎉Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😈😈😈Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(array) {
+  let newArray = [...array];
+  newArray.shift();
+  return newArray;
+};
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
