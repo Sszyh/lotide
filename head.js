e96c6737a2af29b0097f421e9a50f3aebde1bf7a
Sszyh <zhuyer@outlook.com>
2022-06-27 19:54:32 -0700

    add head.js

---
+++ head.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🎉🎉🎉Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😈😈😈Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+//assertEqual("Bootcamp", "Bootcamp");
+//assertEqual(1, 2);
+
+const head = function(array) {
+  let output;
+  if (array === []) {
+    output =  undefined;
+  } else {
+    output = array[0];
+  }
+  return output;
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
