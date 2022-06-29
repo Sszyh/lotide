f880b4e2f086dfda5ac517d55a795e1c5ba9b082
Sszyh <zhuyer@outlook.com>
2022-06-28 22:08:56 -0700

    add modified eqArrays and new file without

--- eqArrays.js
+++ eqArrays.js
@@ -8,15 +8,15 @@ const assertEqual = function(actual, expected) {
 
 
 const eqArrays = function(array1, array2) {
-  let result = true;
+  let result = false;
   if (Array.isArray(array1) && Array.isArray(array2)) {
     for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        result = false;
+      if (array1[i] === array2[i]) {
+        result = true;
       }
     }
   }
   return result;
 };
 // TEST CODE
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+console.log(eqArrays([1, 2, 3], [1, 2, 3]));
---
+++ without.js
@@ -0,0 +1,39 @@
+const eqArrays = function(array1, array2) {
+  let result = false;
+  if (Array.isArray(array1) && Array.isArray(array2)) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] === array2[i]) {
+        result = true;
+      }
+    }
+  }
+  return result;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log("🎉🎉🎉Assertion Passed");
+  } else {
+    console.log("😈😈😈Assertion Failed");
+  }
+};
+
+const without = function(source, itemsToRemove) {
+  let outputArray = [...source];
+  for (let i = 0; i < itemsToRemove.length; i++) {
+    for (let j = 0; j < source.length; j++) {
+      if (itemsToRemove[i] === source[j]) {
+        outputArray.splice(j,1);
+      }
+    }
+  }
+  return outputArray;
+};
+
+console.log(without([1, 2, 3], [1]));
+console.log(without(["1", "2", "3"], [1, 2, "3"]));
+
+const words = ["hello", "world", "lighthouse"];
+console.log(without(words, ["lighthouse"])); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
