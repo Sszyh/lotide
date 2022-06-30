b25e16527d73a35bdec609ffc850969241f6838a
Sszyh <zhuyer@outlook.com>
2022-06-29 19:12:06 -0700

    add countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🎉🎉🎉Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😈😈😈Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(inputString) {
+  const outputObject = {};
+  for (let letter of inputString) {
+    if (letter !== " ") {
+      if (outputObject[letter]) {
+        outputObject[letter] += 1;
+        console.log(outputObject);
+      } else {
+        outputObject[letter] = 1;
+      }
+    }
+  }
+  return outputObject;
+};
+//console.log(outputObject);
+const result = countLetters("lll Hi");
+assertEqual(result.l, 3);
+assertEqual(result.H, 1);
+assertEqual(result.i, 1);
+assertEqual(result[" "], undefined);
