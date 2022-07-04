e69e13da4a2f98ce71038c8e7ad6b2a9553a3a19
Sszyh <zhuyer@outlook.com>
2022-07-04 16:07:32 -0700

    use chai to assert

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,6 +1,12 @@
 const eqArrays = require("../eqArrays.js");
-const assertEqual = require("../assertEqual.js");
+const assert = require("chai").assert;
 
-// TEST CODE
-//console.log(eqArrays([1, 2, 3], [1, 2, 3]));
-assertEqual(eqArrays([1, 2, 3], [1, 2]), false);
+describe("#eqArrays", () => {
+  it("return false for ([1,2,3], [1,2])", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, 2]), false);
+    
+  })
+  it("return true for ([1,2,3], [1,2,3]", () => {
+    assert.strictEqual(eqArrays([1,2,3], [1,2,3]), true);
+  })
+});
