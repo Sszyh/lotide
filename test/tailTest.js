23d2d455ac39bf20a10d32ad4577c57c1c64a2b1
Sszyh <zhuyer@outlook.com>
2022-07-04 16:09:11 -0700

    use chai to assert

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,11 @@
+
+const assert = require("chai").assert;
 const tail = require("../tail.js");
-const assertEqual = require("../assertEqual.js");
 
+describe("#tail", () => {
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+  it("return ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Yo Yo', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
+  
+})
