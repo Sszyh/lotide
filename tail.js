bc44a79ccab94feca7b42c3aae9764fe48a00fa9
Sszyh <zhuyer@outlook.com>
2022-07-04 13:19:41 -0700

    modified tail to remove assertion

--- tail.js
+++ tail.js
@@ -1,10 +1,10 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🎉🎉🎉Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`😈😈😈Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+//const assertEqual = function(actual, expected) {
+//  if (actual === expected) {
+//    console.log(`🎉🎉🎉Assertion Passed: ${actual} === ${expected}`);
+//  } else {
+//    console.log(`😈😈😈Assertion Failed: ${actual} !== ${expected}`);
+//  }
+//};
 
 const tail = function(array) {
   let newArray = [...array];
@@ -12,6 +12,8 @@ const tail = function(array) {
   return newArray;
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+//const words = ["Yo Yo", "Lighthouse", "Labs"];
+//tail(words); // no need to capture the return value since we are not checking it
+//assertEqual(words.length, 3); // original array should still have 3 elements!
+//
+module.exports = tail
