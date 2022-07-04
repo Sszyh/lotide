162b2fab71080fa8345835f37badb902abff0efa
Sszyh <zhuyer@outlook.com>
2022-07-04 16:08:38 -0700

    use chai to assert

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,15 @@
-const assertArraysEqual = require("../assertArraysEqual.js");
+
+
+const assert = require("chai").assert;
 const middle = require("../middle.js");
 
+describe("#middle", () => {
+  it("return 5 for [2,4,5,4,3]", () => {
+    assert.deepEqual(middle([2,4,5,4,3]), [5]);
+  });
 
-assertArraysEqual(middle([2,4,5,4,3]), [5]);
+  it("return [2,3] for [4,3,2,3,8,7]", () => {
+    assert.deepEqual(middle([4,3,2,3,8,7]), [2,3]);
+  
+  });
+});
