0120df683aaba7d6fb81d58f8817aa9e9f4e4235
Sszyh <zhuyer@outlook.com>
2022-07-04 13:03:05 -0700

    modified assertEqual.js just keep the main function

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🎉🎉🎉Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😈😈😈Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+module.exports = assertEqual
