32eb197baf1f98b2988f7973804ef63da5ae7d86
Sszyh <zhuyer@outlook.com>
2022-07-15 12:15:34 -0700

    refacotring findkey using for in

--- findKey.js
+++ findKey.js
@@ -1,23 +1,25 @@
-const findKey = function(object, callback) {
-  let output = "";
-  for(let obj in object) {
-    if(callback(object[obj])) {
-    output = obj;
-    return output;
+// const findKey = function(object, callback) {
+//   let output = "";
+//   for(let obj in object) {
+//     if(callback(object[obj])) {
+//     output = obj;
+//     return output;
+//     }
+//   }
+//   return output;
+// }
+
+
+
+
+const findKey = (obj, cb) => {
+  for (let key in obj) {
+    if (cb(obj[key])) {
+      return key;
     }
   }
-  return output;
-}
-//const findKey = (object, callback) => {}
-
-//findKey({
-//  "Blue Hill": { stars: 1 },
-//  "Akaleri":   { stars: 3 },
-//  "noma":      { stars: 2 },
-//  "elBulli":   { stars: 3 },
-//  "Ora":       { stars: 2 },
-//  "Akelarre":  { stars: 3 }
-//}, x => x.stars === 2)// => "noma"
+  return undefined;
+};
 
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
@@ -27,13 +29,13 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-//assertEqual(findKey({
-//  "Blue Hill": { stars: 1 },
-//  "Akaleri":   { stars: 3 },
-//  "noma":      { stars: 2 },
-//  "elBulli":   { stars: 3 },
-//  "Ora":       { stars: 2 },
-//  "Akelarre":  { stars: 3 }
-//}, x => x.stars === 2), "noma");
+assertEqual(findKey({
+ "Blue Hill": { stars: 1 },
+ "Akaleri":   { stars: 3 },
+ "noma":      { stars: 2 },
+ "elBulli":   { stars: 3 },
+ "Ora":       { stars: 2 },
+ "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma");
 
 module.exports = findKey;
