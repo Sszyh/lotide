5c8c70c879f1e462de9aa3acb9f337510285e705
Sszyh <zhuyer@outlook.com>
2022-07-25 15:10:49 -0700

    refactory findKeyTest and findKey

--- findKey.js
+++ findKey.js
@@ -1,16 +1,3 @@
-// const findKey = function(object, callback) {
-//   let output = "";
-//   for(let obj in object) {
-//     if(callback(object[obj])) {
-//     output = obj;
-//     return output;
-//     }
-//   }
-//   return output;
-// }
-
-
-
 
 const findKey = (obj, cb) => {
   for (let key in obj) {
@@ -21,21 +8,5 @@ const findKey = (obj, cb) => {
   return undefined;
 };
 
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`🎉🎉🎉Assertion Passed: ${actual} === ${expected}`);
-//   } else {
-//     console.log(`😈😈😈Assertion Failed: ${actual} !== ${expected}`);
-//   }
-// };
-
-// assertEqual(findKey({
-//  "Blue Hill": { stars: 1 },
-//  "Akaleri":   { stars: 3 },
-//  "noma":      { stars: 2 },
-//  "elBulli":   { stars: 3 },
-//  "Ora":       { stars: 2 },
-//  "Akelarre":  { stars: 3 }
-// }, x => x.stars === 2), "noma");
 
 module.exports = findKey;
