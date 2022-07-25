3899bbc9f694cfd8dde89cd10289ab6995d2d5ea
Sszyh <zhuyer@outlook.com>
2022-07-25 14:16:57 -0700

    modified countOnly

--- countOnly.js
+++ countOnly.js
@@ -1,10 +1,10 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🎉🎉🎉Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`😈😈😈Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`🎉🎉🎉Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`😈😈😈Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
 
 // const countOnly = function(allItems, itemsToCount) {
 //   const results = {};
