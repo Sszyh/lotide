03dde9cc98ec012012f5992dbb5a8afbd99fadc3
Sszyh <zhuyer@outlook.com>
2022-07-25 14:02:11 -0700

    modified flatten

--- flatten.js
+++ flatten.js
@@ -1,25 +1,25 @@
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`🎉🎉🎉Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`😈😈😈Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// const assertArraysEqual = function(actual, expected) {
+//   if (eqArrays(actual, expected)) {
+//     console.log(`🎉🎉🎉Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`😈😈😈Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
 
-const eqArrays = function(array1, array2) {
-  if (Array.isArray(array1) && Array.isArray(array2)) {
-    if (array1.length !== array2.length) {
-      return false;
-    }
+// const eqArrays = function(array1, array2) {
+//   if (Array.isArray(array1) && Array.isArray(array2)) {
+//     if (array1.length !== array2.length) {
+//       return false;
+//     }
 
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
+//     for (let i = 0; i < array1.length; i++) {
+//       if (array1[i] !== array2[i]) {
+//         return false;
+//       }
+//     }
+//     return true;
+//   }
+// };
 
 // const flatten = function(arr) {
 //   let flattenArr = [];
