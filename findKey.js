774fd0c0b3e6d8a00e07d2c545b2842d587a9974
Sszyh <zhuyer@outlook.com>
2022-07-25 12:33:33 -0700

    remove testcode in the file

--- findKey.js
+++ findKey.js
@@ -21,21 +21,21 @@ const findKey = (obj, cb) => {
   return undefined;
 };
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🎉🎉🎉Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`😈😈😈Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`🎉🎉🎉Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`😈😈😈Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
 
-assertEqual(findKey({
- "Blue Hill": { stars: 1 },
- "Akaleri":   { stars: 3 },
- "noma":      { stars: 2 },
- "elBulli":   { stars: 3 },
- "Ora":       { stars: 2 },
- "Akelarre":  { stars: 3 }
-}, x => x.stars === 2), "noma");
+// assertEqual(findKey({
+//  "Blue Hill": { stars: 1 },
+//  "Akaleri":   { stars: 3 },
+//  "noma":      { stars: 2 },
+//  "elBulli":   { stars: 3 },
+//  "Ora":       { stars: 2 },
+//  "Akelarre":  { stars: 3 }
+// }, x => x.stars === 2), "noma");
 
 module.exports = findKey;
