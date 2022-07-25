4bf8390dd9d2fa35277a6bdd1833d61d366cda63
Sszyh <zhuyer@outlook.com>
2022-07-25 15:08:56 -0700

    refactory eqObjectsTest and eqObjects

--- eqObjects.js
+++ eqObjects.js
@@ -1,11 +1,3 @@
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`🎉🎉🎉Assertion Passed: ${actual} === ${expected}`);
-//   } else {
-//     console.log(`😈😈😈Assertion Failed: ${actual} !== ${expected}`);
-//   }
-// };
-
 const eqObjects = function(object1, object2) {
   let keyArray1 = Object.keys(object1);
   let keyArray2 = Object.keys(object2);
@@ -39,25 +31,7 @@ const eqArrays = function(array1, array2) {
   }
 };
 
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-console.log(eqObjects(ab, ba)); // => true
-
-//const abc = { a: "1", b: "2", c: "3" };
-//eqObjects(ab, abc); // => false
-
-//assertEqual(eqObjects(ab, ba), true);
-//assertEqual(eqObjects(ab, abc), false);
-
-//const cd = { c: "1", d: ["2", 3]};
-//const dc = { d: ["2", 3], c: "1" };
-//console.log(eqObjects(cd, dc)); // => true////
-
-//const cd2 = { c: "1", d: ["2", 3, 4] };
-//console.log(eqObjects(cd, cd2)); // => false////
-//
-
-//assertEqual(eqObjects(cd, dc), true);
-//assertEqual(eqObjects(cd, cd2), false);
 
 module.exports = eqObjects;
+
+
--- test/eqObjectsTest.js
+++ test/eqObjectsTest.js
@@ -2,9 +2,24 @@ const assert = require("chai").assert;
 const eqObjects = require("../eqObjects");
 
 describe("#eqObjects", () => {
-  it("return true for two object", () => {
+  it("return true for two objects", () => {
     const ab = { a: "1", b: "2" };
     const ba = { b: "2", a: "1" };
     assert.strictEqual(eqObjects(ab, ba), true);
+  });
+  it("return false for two objects", () => {
+    const ab = { a: "1", b: "2" };
+    const abc = { a: "1", b: "2", c: "3" };
+    assert.strictEqual(eqObjects(ab, abc), false);
+  });
+  it("return ture for two objects", () => {
+    const cd = { c: "1", d: ["2", 3]}
+    const dc = { d: ["2", 3], c: "1" };
+    assert.strictEqual(eqObjects(cd, dc), true);
+  });
+  it("return ture for two objects", () => {
+    const cd = { c: "1", d: ["2", 3]}
+    const cd2 = { c: "1", d: ["2", 3, 4] };
+    assert.strictEqual(eqObjects(cd, cd2), false);
   });
 }) 
