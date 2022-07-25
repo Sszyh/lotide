27a64a6d948d264438957f5fc4f8ffc4873b6b04
Sszyh <zhuyer@outlook.com>
2022-07-25 14:01:15 -0700

    refactory flatten and flatten test

--- flatten.js
+++ flatten.js
@@ -50,7 +50,8 @@ const flatten = (nestArr) => {
   return array;
 }
 
-console.log(flatten([1, 2, [3, 4], 5, [6]]));
+//console.log(flatten([1, 2, [3, 4], 5, [6]]));
 //console.log(flatten([1, 2, [[3, 4], 5], [6]]));
 
 
+module.exports = flatten;
